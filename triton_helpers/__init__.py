a74676b24fbaedc57d7f3bc67a59ed42e21ab0d6
djcarrad <djcarrad@gmail.com>
2025-08-27 20:36:14 +0200

    create connect_all for triton, magnet and qswitches

--- triton_helpers/__init__.py
+++ triton_helpers/__init__.py
@@ -1,1 +1,2 @@
 from .qswitch_helpers import connect_qswitches
+from .connect_helpers import connect_all
---
+++ triton_helpers/connect_helpers.py
@@ -0,0 +1,22 @@
+from qcodespp.instrument_drivers.oxford.triton import Triton
+from qcodespp.instrument_drivers.oxford.mercuryiPS_serial import MercuryiPS_120
+from triton_helpers.qswitch_helpers import connect_qswitches
+
+def connect_all(which):
+    """
+    Connect to all stationary objects.
+
+    Args:
+        which (str): The connector used for the qswitches, either 'inner' or 'outer'.
+
+    Returns:
+        tuple: A tuple containing the connected instruments.
+
+    Usage:
+        triton, magnet, qs1, qs2 = connect_all('outer')
+    """
+    qs1, qs2 = connect_qswitches(which)
+    triton = Triton(name='triton', address='169.254.100.101', port=33576)
+    magnet = MercuryiPS_120(name='magnet', address='com7')
+
+    return triton, magnet, qs1, qs2
