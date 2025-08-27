f89010448be62685291fb2e6da98b824ecd1cb67
djcarrad <djcarrad@gmail.com>
2025-08-27 23:19:18 +0200

    fix triton_helper docstrings

--- triton_helpers/triton_helpers.py
+++ triton_helpers/triton_helpers.py
@@ -1,11 +1,11 @@
 def high_temp_mode(parameter,swap_temp=1.25):
     '''
-    Switch cryostat to a state safe for temperatures above approx. 1.25 K during a loop.
+    Switch cryostat to a safe state for high temperatures during a loop if temperature exceeds approx. 1.25 K.
 
     Args:
         parameter (Parameter): the parameter to use for comparison. 
             Should be triton.MC, triton.T8, triton.T5 or triton.pid_setpoint
-        swap_temp (float): The setpoint temperature in K at which to switch to high temperature mode.
+        swap_temp (Opt, float): The setpoint temperature in K at which to switch to high temperature mode.
     Usage:
         station.set_measurement(qc.Task(high_temp_mode,triton,1.26))
     '''
@@ -15,7 +15,7 @@ def high_temp_mode(parameter,swap_temp=1.25):
 
 def high_temp_mode_now(triton):
     '''
-    Switch cryostat to a safe state for high temperatures now.
+    Immediately switch cryostat to a safe state for high temperatures.
 
     Args:
         triton (Instrument): the triton instance.
